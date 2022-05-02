05dd4d6db42fccaf850866f965ae90c7488add96
洛栀 <admin@luozhinet.com>
2022-05-02 22:24:34 +0800

    About me

---
+++ js/pt_404_2d5f4d27bda3345d946a9d6ceba4355f.js
@@ -0,0 +1,15 @@
+(function() {
+    var c8g = NEJ.P,
+        a8i = c8g("nej.e"),
+        h8b = c8g("nej.v"),
+        F8x = c8g("nm.m"),
+        N9E = c8g("nm.m"),
+        b8h, L9C;
+    F8x.bZu1x = NEJ.C();
+    b8h = F8x.bZu1x.M9D(F8x.dm9d);
+    b8h.bL9C = function() {
+        this.bT9K()
+    };
+    a8i.cW9N("template-box");
+    new N9E.bZu1x
+})()
